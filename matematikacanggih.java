e07c9074b6cef5e625fe1deebba484966b55870c
ImeldaNurAzhilah <imeldanur1711@gmail.com>
2024-11-24 21:19:03 +0700

    Add files via upload

---
+++ matematikacanggih.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modulus;
+
+/**
+ *
+ * @author ASUS
+ */
+public class matematikacanggih extends matematika{
+    public int modulus(int angka1, int angka2){
+        return angka1 % angka2;
+        
+    }
+    
+    
+}
+    
